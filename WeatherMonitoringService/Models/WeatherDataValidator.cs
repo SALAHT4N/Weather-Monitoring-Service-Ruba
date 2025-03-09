efe8b0909c9c7ffd84e3e03d94fac895dfca23e4
Ruba <s12028675@stu.najah.edu>
2025-03-09 13:44:03 +0200

    Refactor Code

--- WeatherMonitoringService/Models/WeatherDataValidator.cs
+++ WeatherMonitoringService/Models/WeatherDataValidator.cs
@@ -7,9 +7,13 @@ public class WeatherDataValidator : AbstractValidator<WeatherData>
     public WeatherDataValidator()
     {
         RuleFor(weatherData => weatherData.Temperature)
-            .NotEmpty().WithMessage("Please specify a valid temperature");
+            .NotEmpty().
+            WithMessage("Please specify a valid temperature");
+        
         RuleFor(weatherData => weatherData.Location)
-            .NotEmpty().WithMessage("Please specify a valid location");
+            .NotEmpty()
+            .WithMessage("Please specify a valid location");
+        
         RuleFor(weatherData => weatherData.Humidity)
             .NotEmpty().WithMessage("Please specify a valid humidity");
     }
